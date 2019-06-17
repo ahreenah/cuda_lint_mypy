b2fc8e72ec2b867d3b3b64aea66cb51d74fce72d
medvosa <medvosa@yandex.ru>
2019-06-17 14:03:15 +0300

    irst commit

---
+++ linter.py
@@ -0,0 +1,43 @@
+"""This module exports the Gcc plugin class."""
+
+from cuda_lint import Linter, util
+from cudatext import * 
+
+
+class MyPy(Linter):
+
+    """Provides an interface to mypy"""
+    cmd = 'mypy'
+    executable = 'mypy'
+    multiline = True
+    syntax = ('Python')
+    regex = (
+        r'(?P<stdin>.*):(?P<line>\d+): error:(?P<message>.*)'
+    )
+    base_cmd = (
+    ''
+    )
+    tempfile_suffix = 'py'
+
+
+    def split_match(self, match):
+        print(match)
+   
+        """Return the components of the error."""
+        split_match = super(MyPy, self).split_match(match)
+
+        match, line, col, error, warning, message, near = split_match
+        return match, line, 0, '', '', message, ''
+        
+
+
+    def cmd(self):
+    
+        """Return the command line to execute."""
+        result = self.executable + ' ' + self.base_cmd
+
+        code_type='python'
+
+        result += ''
+
+        return result
